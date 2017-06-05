ee296c596988c8b9bc5f1e58bf6713dd380b52d0
Aperocky <zl131@duke.edu>
2017-06-05 15:48:41 -0400

    The skeleton of the process is complete

--- src/Task.java
+++ src/Task.java
@@ -32,11 +32,14 @@ public class Task implements Comparable<Task> {
 	private double beginTime;
 	//why did you need an endTime?
 	private double endTime;
-	//opNums?
 	private int[] opNums;
 	private loadparam parameters;
 
 // Mutators
+	
+	public void setEndTime(double time){
+		endTime = time;
+	}
 
 	public void setBeginTime(double time){
 		beginTime = time;
